b2fda5ccf0c9287d7271a11ca5c8e72eb9336395
Ievgen Nakonechnyi <vnd.ievgen.nakonechnyi@bcgdv.com>
2018-12-26 10:53:38 -0800

    root pom.xml cleaned

--- src/test/java/com/david/corp/step_definitions/WebDriverFactory.java
+++ src/test/java/com/david/corp/step_definitions/WebDriverFactory.java
@@ -5,12 +5,10 @@ import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class WebDriverFactory {
